9782a93dfa4a187e94bd9bba78bc022536971ded
joaoomoraiss <jooaommorais@gmail.com>
2024-05-11 11:58:35 -0300

    flapybird.py

---
+++ venv/flapybird.py
@@ -0,0 +1,73 @@
+import pygame
+import os
+import random
+
+#tela do jogo
+
+TELA_LARGURA = 500
+TELA_ALTURA = 800
+
+#imagens do jogo
+
+IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('img', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transorm.scale2x(pygame.image.load(os.path.join('img', 'bg.png')))
+IMAGEM_PASSARO = [
+    pygame.transorm.scale2x(pygame.image.load(os.path.join('img', 'bird1.png'))),
+    pygame.transorm.scale2x(pygame.image.load(os.path.join('img', 'bird2.png'))),
+    pygame.transorm.scale2x(pygame.image.load(os.path.join('img', 'bird3.png')))]
+
+#font da pontuação
+pygame.font.init()
+FONTE_PONTOS = pygame.font.SysFont('arial', 50)
+
+class Passaro:
+    IMGS = IMAGEM_PASSARO
+    #ANIMAÇÃO DO PASSARO
+    ROTAÇAO_MAXIMA = 25
+    VELOCIDADE_ROTACAO = 20
+    TEMPO_ANIMACAO = 5
+
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+        self.angulo = 0
+        self.velocidade = 0
+        self.altura = self.y
+        self.tempo = 0
+        self.contagem_imagem = 0
+        self.imagem = IMGS[0]
+
+    def pular(self):
+        self.velocidade = -10.5
+        self.tempo = 0
+        self.altura = self.y
+
+    def mover(self):
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
+
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0:
+            deslocamento -= 2
+
+        self.y += deslocamento
+
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTAÇAO_MAXIMA:
+                self.angulo = self.ROTAÇAO_MAXIMA
+        else:
+            if self.angulo < -90:
+                self.angulo -= self.VELOCIDADE_ROTACAO
+        
+    def desenhar(self):
+
+
+class Cano:
+    pass
+
+
+class Chao:
+    pass
+
