4bf620310398941139150ef686e6c9619d81a130
joaoomoraiss <jooaommorais@gmail.com>
2024-06-06 14:16:30 -0300

    Update flapybird.py
    class cano

--- venv/flapybird.py
+++ venv/flapybird.py
@@ -88,10 +88,57 @@ class Passaro:
         posicao_centro_imagem = self.imagem.get_rect(topleft=(self.x, self.y)).center
         retangulo = imagem_rotacionada.get_rect(center=posicao_centro_imagem)
         tela.blit(imagem_rotacionada, retangulo.topleft)
+    
+
+    #hitbox do passaro
+    def get_mask(self):
+        pygame.mask.from_surface(self.imagem)
 
 class Cano:
-    pass
+    DISTANCIA = 200
+    VELOCIDADE = 5
 
+    def __init__(self, x):
+        self.x = x
+        self.altura = 0
+        self.posicao_topo = 0
+        self.posicao_base = 0
+        self.CANO_TOPO = pygame.transform(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO
+        self.passou = False
+        self.definir_altura()
+
+    #local aleatorio de onde o cano vai aparecer
+    def definir_altura(self):
+        self.altura = random.range(50,450)
+        self.posicao_base = self.altura - self.CANO_TOPO.get_height()
+        self.posicao_topo = self.altura + self.DISTANCIA
+
+    #cano se movendo
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    #aparecer na tela
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO, (self.x, self.posicao_topo))
+        tela.blit(self.CANO_BASE, (self.x, self.posicao_base))
+
+    #quando o passaro bate no cano
+    def colidir(self, passaro):
+        passaro_mask = passaro.get_mask()
+        topo_mask = pygame.mask.from_surface(self.CANO_TOPO)
+        base_mask = pygame.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passaro.x, self.posicao_topo - round(passaro.y))
+        distancia_base = (self.x - passaro.x, self.posicao_base, - round(passaro.y))
+
+        topo_ponto = passaro_mask.overlap(topo_mask, distancia_topo)
+        base_ponto = passaro_mask.overlap(base_mask, distancia_base)
+
+        if base_ponto or topo_ponto:
+            return True
+        else:
+            return False
 
 class Chao:
     pass
